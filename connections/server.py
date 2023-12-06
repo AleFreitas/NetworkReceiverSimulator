fbc2d07a4f71b62ad3552a24c3aaeeeb8f1a265d
Vitor <190118326@aluno.unb.br>
2023-12-05 21:35:30 -0300

    feat: Atualização das conexões de server e client

--- connections/server.py
+++ connections/server.py
@@ -1,25 +1,38 @@
 import socket
 import threading
 import pickle
-def connect(dst):
+
+def config():
     HEADER = 4
-    PORT = {'transmissor': 5050, 'receptor': 5051, 'interface': 5052}
-    FORMAT = "utf-8"
+    PORT = 5051
     SERVER = socket.gethostbyname(socket.gethostname())
-    ADDR = (SERVER, PORT.get(dst))
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    clientData = {"HEADER": HEADER, "FORMAT": FORMAT, "client": client}
-    return clientData
+    ADDR = (SERVER, PORT)
+    DISCONNECT_MESSAGE = "Desconectar"
+    FORMAT = "utf-8"
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    serverData = {"HEADER": HEADER, "FORMAT": FORMAT, "server": server, "ip": SERVER, "DISCONNECT_MESSAGE": DISCONNECT_MESSAGE}
+    return serverData
 
-def disconnect(clientData):
-    DISCONNECT_MESSAGE = 'Desconectar'
-    send(DISCONNECT_MESSAGE, clientData)
+def handle_client(conn, addr, serverData):
+    print('Nova Conexão!')
+    connect = True
+    while connect:
+        msg_lenght = conn.recv(serverData['HEADER']).decode(serverData['FORMAT'])
+        if msg_lenght:
+            msg_lenght = int(msg_lenght)
+            serialized_msg = conn.recv(msg_lenght)
+            msg = pickle.loads(serialized_msg)
+            if msg == serverData['DISCONNECT_MESSAGE']:
+                connect = False
+            print(f"{addr} {msg}")
+    conn.close()
 
-def send(msg, clientData):
-    serialized_msg = pickle.dumps(msg)
-    msg_lenght = len(serialized_msg)
-    send_lenght = str(msg_lenght).encode(clientData['FORMAT'])
-    send_lenght += b' '*(clientData['HEADER'] - len(send_lenght))
-    clientData['client'].send(send_lenght)
-    clientData['client'].send(serialized_msg)
+def start(serverData):
+    serverData['server'].listen()
+    print(f"O servidor está ouvindo em {serverData['ip']}")
+    while True:
+        conn, addr =  serverData['server'].accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr, serverData))
+        thread.start()
+        print(f"Conexões Ativas {threading.activeCount() - 1}")
