91c94e39ba42f34f78a6225728763e2e8dd95a18
Alexandre de Freitas Souza <alexandre.souza@pixforce.ai>
2023-12-09 17:12:58 -0300

    undo: parity check

--- main.py
+++ main.py
@@ -25,42 +25,13 @@ def handle_client(conn, addr, serverData):
         if msg_lenght:
             msg_lenght = int(msg_lenght)
             serialized_msg = conn.recv(msg_lenght)
-            # Gera uma chance de 10% de erro de paridade 
-            if random.random() < 0.1:
-                _msg = induce_parity_error(serialized_msg)
-            # Verifica o bit de paridade
-            if check_parity(serialized_msg):
-                msg = pickle.loads(serialized_msg[:-1])  # Remove o bit de paridade antes de desserializar
-                
-                if msg == serverData['DISCONNECT_MESSAGE']:
-                    connect = False
-                    codificar(lista_de_mensagens)
-                else:
-                    lista_de_mensagens.append(msg)
-                    print(f"{addr} {msg} (Mensagem recebida com sucesso)")
-            else:
-                print(f"{addr} Erro de bit de paridade detectado")
+            msg = pickle.loads(serialized_msg)
+            if msg == serverData['DISCONNECT_MESSAGE']:
+                connect = False
+                codificar(lista_de_mensagens)
+            lista_de_mensagens.append(msg)
 
     conn.close()
-
-def induce_parity_error(serialized_msg):
-    # Converte a mensagem serializada para uma lista de bytes
-    msg_bytes = bytearray(serialized_msg)
-
-    # Inverte o último bit na mensagem
-    last_byte_index = len(msg_bytes) - 1
-    msg_bytes[last_byte_index] ^= 1  # Inverte o último bit usando operador XOR
-
-    # Retorna a mensagem com o erro de bit de paridade
-    return bytes(msg_bytes)
-
-
-def check_parity(data):
-    # Conta o número de bits 1 na mensagem
-    number_of_ones = sum(map(int, bin(int.from_bytes(data, byteorder='big'))[2:]))
-    
-    # Retorna True se o número de bits 1 for par, indicando paridade correta
-    return number_of_ones % 2 == 0
 
 def start(serverData):
     serverData['server'].listen()
